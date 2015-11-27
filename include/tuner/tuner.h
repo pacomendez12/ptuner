2baa170973ad9532c1f8dac5edc71e70b104937e
Francisco Mendez <pacomendez12@gmail.com>
2015-11-27 17:33:54 -0600

    fft: implemented fft, not tested yet
    only advancing with fft, it's needed to test if is working, maybe with
    a real guitar
    
    Signed-off-by: Francisco Mendez <pacomendez12@gmail.com>

--- include/tuner/tuner.h
+++ include/tuner/tuner.h
@@ -18,7 +18,8 @@ using namespace Sound_system;
 enum {
 	/* it needs at least 16384 samples to get at most ~8000 hz frequency */
 	TUNER_SAMPLES = 16384,
-	NUM_BLOQUES =		64
+	DEFAULT_OVERSAMPLING = 64,
+	DEFAULT_DOWNSAMPLE = true,
 };
 
 
@@ -47,6 +48,11 @@ class Tuner {
 	unsigned int sample_rate;
 	int sound_system_buffer_size;
 
+	unsigned int oversampling;
+	unsigned int peak_number;
+
+	/* the goal */
+	double frequency;
 
 	/* dmax is used for normalization */
 	double dmax;
@@ -55,6 +61,25 @@ class Tuner {
 	/* Tnuning parameters */
 	bool useFilter;
 	Filters filter;
+	bool downsample;
+
+
+	/* valid data from fft are in the size of 0 - buffer lenght * 3 /8 */
+#define  OUTPUT_BUFFER_SIZE TUNER_SAMPLES * 3 / 8
+	double fps = sample_rate;
+
+	/* auxiliar buffers */
+	double xa[OUTPUT_BUFFER_SIZE];
+	double xp[OUTPUT_BUFFER_SIZE];
+	double xf[OUTPUT_BUFFER_SIZE];
+	double dx[OUTPUT_BUFFER_SIZE];
+
+
+	/* buffers for downsampling */
+	double x2[OUTPUT_BUFFER_SIZE / 2];
+	double x3[OUTPUT_BUFFER_SIZE / 3];
+	double x4[OUTPUT_BUFFER_SIZE / 4];
+	double x5[OUTPUT_BUFFER_SIZE / 5];
 
 
 
@@ -71,7 +96,26 @@ class Tuner {
 	void callbackData(double * data, int size_data, void * dummy);
 
 
+	/* main methods */
+	void postFftProccess(complex * complex_buffer, int buffer_size);
+	void downSampling();
+
+
+	double getFundamentalFrequency() {
+		return frequency;
+	}
+
+
 };
 
+
+
+#define DOWNSAMPLING_METHOD(n) \
+	for (int i = 0; i < ARRAY_SIZE(x##n); i++) { \
+		x##n[i] = 0.0; \
+		for (int j = 0; j < n; j++) { \
+			x##n[i] += xa[(i * n) + j] / (double)n; \
+		} \
+	}\
 
 #endif /* end of include guard: TUNER_H */
